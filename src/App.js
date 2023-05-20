567a15b6b2f21a171ab54deaf6d2ee6af10729f3
toddwiggins2 <toddwiggins2@gmail.com>
2023-05-19 20:50:42 -0400

    Initial React CRUD app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,140 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+import "./App.css";
+
+const list = [
+  {
+    id: 1,
+    firstname: `Todd`,
+    lastname: `Wiggins`,
+    age: `40`,
+  },
+  {
+    id: 2,
+    firstname: `Kristen`,
+    lastname: `Wiggins`,
+    age: `43`,
+  },
+  {
+    id: 3,
+    firstname: `Jeff`,
+    lastname: `Wiggins`,
+    age: `68`,
+  },
+  {
+    id: 4,
+    firstname: `Vickey`,
+    lastname: `Wiggins`,
+    age: `67`,
+  },
+];
+
+// function App() {
+//   const listitems = list.map((list) => (
+//     <div key={list.id} className="list-div">
+//       <ul className="list-todos">
+//         {list.firstname} {list.lastname} age {list.age}
+//       </ul>
+//       <button className="btn-delete">Delete</button>
+//     </div>
+//   ));
 
 function App() {
+  const [listItems, setListItems] = useState(list);
+  const [inputBox, setInputBox] = useState({
+    firstname: "",
+    lastname: "",
+    age: "",
+  });
+
+  const handleDelete = (id) => {
+    const updatedListItems = listItems.filter((item) => item.id !== id);
+    setListItems(updatedListItems);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setInputBox((prevInputBox) => ({
+      ...prevInputBox,
+      [name]: value,
+    }));
+  };
+
+  const handleAdd = (newUser) => {
+    newUser = { ...newUser, id: uuidv4() };
+    setListItems((prevListItems) => [...prevListItems, newUser]);
+    setInputBox({
+      firstname: "",
+      lastname: "",
+      age: "",
+    });
+    console.log(listItems);
+  };
+
+  const handleUpdate = (item) => {
+    setListItems((prevArray) => {
+      const newArray = prevArray.map((obj) => {
+        if (obj.id === item.id) {
+          return { ...obj, ...inputBox };
+        }
+        return obj;
+      });
+      return newArray;
+    });
+  };
+
+  const listItemsMap = listItems.map((item) => (
+    <div key={item.id} className="list-div">
+      <ul className="list-todos">
+        {item.firstname} {item.lastname} age {item.age}
+      </ul>
+      <div style={{ display: "flex", gap: "0.5em" }}>
+        <button onClick={() => handleDelete(item.id)} className="btn-delete">
+          Delete
+        </button>
+        <button onClick={() => handleUpdate(item)} className="btn-delete">
+          Update
+        </button>
+      </div>
+    </div>
+  ));
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="search-div">
+          <div style={{ display: "flex", gap: ".25em" }}>
+            <input
+              className="input-box"
+              name="firstname"
+              value={inputBox.firstname}
+              type="text"
+              onChange={handleChange}
+              placeholder="First Name"
+            ></input>
+            <input
+              className="input-box"
+              name="lastname"
+              value={inputBox.lastname}
+              type="text"
+              placeholder="Last Name"
+              onChange={handleChange}
+            ></input>
+            <input
+              className="input-box"
+              name="age"
+              value={inputBox.age}
+              type="text"
+              placeholder="Age"
+              onChange={handleChange}
+            ></input>
+          </div>
+          <button className="btn-add" onClick={() => handleAdd(inputBox)}>
+            Add Item
+          </button>
+        </div>
+        {listItemsMap}
       </header>
     </div>
   );
